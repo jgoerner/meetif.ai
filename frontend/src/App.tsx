67b3b0e81b21f8fa54d8720bf983b367241a82af
jgoerner <joshua.goerner@gmail.com>
2019-10-11 19:10:11 +0200

    Initial Frontend Skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <p>Nothing here to see</p>
+    </div>
+  );
+}
+
+export default App;
